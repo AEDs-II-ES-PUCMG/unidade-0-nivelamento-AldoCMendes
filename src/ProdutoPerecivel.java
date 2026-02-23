678fe48579160da03017d1ec3dbc697d16e5841f
AldoCMendes <aldoconstantinomendes@gmail.com>
2026-02-23 13:55:23 -0300

    Removendo imports

--- src/ProdutoPerecivel.java
+++ src/ProdutoPerecivel.java
@@ -1,7 +1,5 @@
-import java.text.NumberFormat;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.time.temporal.ChronoUnit;
 
 public class ProdutoPerecivel extends Produto {
 
