3347e58f3b5db1d65916c185c82148e8aa427c37
AldoCMendes <aldoconstantinomendes@gmail.com>
2026-02-23 13:54:43 -0300

    formatando

--- src/ProdutoPerecivel.java
+++ src/ProdutoPerecivel.java
@@ -1,36 +1,37 @@
 import java.text.NumberFormat;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 
 public class ProdutoPerecivel extends Produto {
 
-	private static final double DESCONTO = 0.25;
-	private static final int PRAZO_DESCONTO = 7;
-	private LocalDate dataDeValidade;
+    private static final double DESCONTO = 0.25;
+    private static final int PRAZO_DESCONTO = 7;
+    private LocalDate dataDeValidade;
 
-	public ProdutoPerecivel(String desc, double precoCusto, double margemLucro, LocalDate validade) {
-		super(desc, precoCusto, margemLucro);
+    public ProdutoPerecivel(String desc, double precoCusto, double margemLucro, LocalDate validade) {
+        super(desc, precoCusto, margemLucro);
         if (validade.isBefore(LocalDate.now())) {
             throw new IllegalArgumentException("Data de validade não pode ser anterior à data atual.");
         }
-    
-		this.dataDeValidade = validade;
-	}
 
-	@Override
-	public double valorDeVenda() {
-		
+        this.dataDeValidade = validade;
+    }
+
+    @Override
+    public double valorDeVenda() {
+
         double desconto = 0.0;
         int diasValidade = LocalDate.now().until(dataDeValidade).getDays();
         if (diasValidade <= PRAZO_DESCONTO) {
             desconto = DESCONTO;
         }
         return (precoCusto * (1.0 + margemLucro)) * (1.0 - desconto);
-        
-	}
 
-	@Override
-	public String toString() {
+    }
+
+    @Override
+    public String toString() {
 
         DateTimeFormatter formatador = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
@@ -38,5 +39,5 @@ public class ProdutoPerecivel extends Produto {
         dados += " - Validade: " + formatador.format(dataDeValidade);
 
         return dados;
-	}
+    }
 }
