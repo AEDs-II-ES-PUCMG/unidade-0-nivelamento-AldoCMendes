9cdd641e004e28df4da97f77ea482d32d7aed32b
Aldo Constantino Mendes <1548915@sga.pucminas.br>
2026-03-09 12:16:13 -0300

    Atividade 09/03

---
+++ src/ItemDePedido.java
@@ -0,0 +1,37 @@
+public  class ItemDePedido {
+
+    // Atributos encapsulados
+    private Produto[] produto;
+    private int quantidade;
+    private double precoVenda;
+
+    /**
+     * Construtor da classe ItemDePedido.
+     * O precoVenda deve ser capturado do produto no momento da criação do item,
+     * garantindo que alterações futuras no preço do produto não afetem este pedido.
+     */
+    public ItemDePedido(Produto[] produtos, int quantidade, double precoVenda) {
+        this.quantidade  = quantidade;
+        this.produto =  produtos ;
+        this.precoVenda = precoVenda;
+    }
+
+
+    
+
+    public double calcularSubtotal() {
+        return 0;
+    }
+
+    // --- Sobrescrita do método equals ---
+
+    /**
+     * Compara a igualdade entre dois itens de pedido.
+     * A regra de negócio define que dois itens são iguais se possuírem o mesmo Produto.
+     */
+    @Override
+    public boolean equals(Object obj) {
+        Pedido outro = (Pedido)obj;
+        return this.produto.equals(outro.produto);
+    }
+}
